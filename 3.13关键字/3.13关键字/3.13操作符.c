06d2b5ea867f53995299e9ede9636a29a6223cbc
Dale smith <yqyq20051313@outlook.com>
2024-03-27 08:36:03 +0800

    test

--- 3.13关键字/3.13关键字/3.13操作符.c
+++ 3.13关键字/3.13关键字/3.13操作符.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main()
-{
+//int main()
+//{
 	//int a = 7 / 2;//除号两端都是整数，执行的是整数除法，只要一方是浮点数，执行浮点数
 	//float c = 7 / 2.0;
 	//int b = 7 % 2;
@@ -9,6 +9,7 @@ int main()
 	//printf("%.1f\n", c);//.数字，确认位数
 	//printf("%d\n", b);//取模（余数。两端只能是整数
 	//return 0;
+
 
 	//int a = 0;//初始化
 	//a = 10;//赋值
@@ -21,6 +22,7 @@ int main()
 	//printf("%d\n", sizeof a);//字节
 
 	//int arr[10] = { 0 };
+	//printf("%d\n", sizeof(arr));//整个数组的大小
 
 
 
@@ -32,15 +34,57 @@ int main()
 	//int b = a++;//++后置，先使用a的值，后++
 	////int b=a,a=a+1
 
-	int a = 10;
-	int b = ++a;//+++前置，先++，后使用
-	//a=a+1，b=a
+	//int a = 10;
+	//int b = ++a;//+++前置，先++，后使用
+	////a=a+1，b=a
 
-	int c = 10;
-	//int d = c--;
-	int d = --c;
-	printf("%d\n", d);
-	printf("%d\n", c);
+	//int c = 10;
+	////int d = c--;
+	//int d = --c;
+	//printf("%d\n", d);
+	//printf("%d\n", c);
+	//return 0;
+	
+
+
+	//强制转换,括号加类型
+//	int a = (int)3.14;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+
+//==为等于
+//int main()
+//{
+//	int a = 10;
+//	if (a = 3)//赋值
+//	{
+//		printf("hehe");//非零为真
+//	}
+//	return 0;
+//}
+
+
+
+int main()
+{
+	//逻辑与&&，一边为零为假
+	//逻辑或||
+
+	/*int a = 0;
+	int b = 20;
+	if (a && b)
+	{
+		printf("hehe");
+	}*/
+
+	int a = 0;
+	int b = 20;
+	if (a || b)
+	{
+		printf("hehe");
+	}
 	return 0;
-
 }
